afea296cae9aabb6a0e22bb0aa97fa80b938eed5
KingK955 <13515348707@163.com>
2020-10-12 21:27:14 +0800

    测试

--- src/main/java/com/sdjzu/graduation/project/GraduationProjectApplication.java
+++ src/main/java/com/sdjzu/graduation/project/GraduationProjectApplication.java
@@ -7,7 +7,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan(basePackages = "com.sdjzu.graduation.project.mapper")
 public class GraduationProjectApplication {
-
+    //测试
     public static void main(String[] args) {
         SpringApplication.run(GraduationProjectApplication.class, args);
     }
